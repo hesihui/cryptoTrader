e22ad5b31ec826227b6d53b144401a2f663640e8
Sihui He <sardarbinarytree@gmail.com>
2022-03-30 17:06:23 -0400

    coin simpligy

---
+++ src/main/java/cryptoTrader/utlis/coinSimplify.java
@@ -0,0 +1,32 @@
+package cryptoTrader.utlis;
+import java.util.*;
+
+public class coinSimplify {
+	
+	Hashtable<String, String> cryptoCoin = new Hashtable<String, String>();
+	public coinSimplify() {
+		// TODO Auto-generated constructor stub
+		cryptoCoin.put("BTC","bitcoin");
+		cryptoCoin.put("ETH","ethereum");
+		cryptoCoin.put("USDT","tether");
+		cryptoCoin.put("BNB","bnb");
+		cryptoCoin.put("USDC","usd coin");
+		cryptoCoin.put("XRP","xrp");
+		cryptoCoin.put("ADA","cardano");
+		cryptoCoin.put("SOL","solana");
+		cryptoCoin.put("LUNA","terra");
+		cryptoCoin.put("AVAX","avalanche");
+		cryptoCoin.put("DOT","polkadot");
+		cryptoCoin.put("DOGE","dogecoin");
+		cryptoCoin.put("BUSD","binance usd");
+		cryptoCoin.put("UST","terra usd");
+		cryptoCoin.put("SHIB","shiba inu");
+		cryptoCoin.put("MATIC","polygon");
+		cryptoCoin.put("WBTC","wrapped bitcoin");
+		cryptoCoin.put("CRO","cronos");
+		cryptoCoin.put("DAI","dai");
+		cryptoCoin.put("NEAR","near protocol");
+		
+	}
+
+}
